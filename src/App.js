7c33113c50f59a6e6108f4efc494440f9e59dc5f
StevenS125 <sts12589@gmail.com>
2018-10-19 14:22:04 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+//components
+
+
+
+
+//modules
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
