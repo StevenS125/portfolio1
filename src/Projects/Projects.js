304f8767eb31ed23a23e2cb9adf6340facd957e7
StevenS125 <sts12589@gmail.com>
2018-10-30 21:31:46 -0400

    finishing touches

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -15,26 +15,27 @@ class Projects extends Component {
                 projImg: "",
                 projDesc: "",
                 projTitle: "",
-                altTitle: ""
+                altTitle: "",
+                gitLink: "",
+                siteLink: ""
             }
     }
 
 
-// creates feature box div 
+// creates feature box div and is updated by functions to change state
     openBox = () => {
         return (
           <Animated animationIn="fadeIn" animationInDelay={0} isVisible={true}>
                 <div className={styles.featureBox}>
-                <div className={styles.boxDesc}>
-                <div className={styles.projTop}>{this.state.projTitle}</div>
-                <img src={this.state.projImg} className={styles.dispImg} alt={this.state.altTitle}></img>
-                <div className={styles.projDescript}>{this.state.projDesc}</div>
-                <div className={styles.btnBox}>
-                <div className={styles.projBottom}><button className={styles.projBtn}>View Site</button></div> 
-                <div className={styles.projBottom}><button className={styles.projBtn}>Github</button></div>
-                </div>
-                </div>
-                     
+                    <div className={styles.boxDesc} onClick={this.toggleBox}>
+                        <div className={styles.projTop}>{this.state.projTitle}</div>
+                            <img src={this.state.projImg} className={styles.dispImg} alt={this.state.altTitle}></img>
+                                <div className={styles.projDescript}>{this.state.projDesc}</div>
+                                    <div className={styles.btnBox}>
+                                <div className={styles.projBottom}><a href={this.state.siteLink} target="blank"><button className={styles.projBtn}>View Site</button></a></div> 
+                                <div className={styles.projBottom}><a href={this.state.gitLink} target="blank"><button className={styles.projBtn}>Github</button></a></div>
+                                    </div>
+                    </div>      
                 </div>
           </Animated>
                ); 
@@ -67,7 +68,10 @@ class Projects extends Component {
             + "a Node engine. The basis for this project was a marketing site to attract external"
             + "businesses and show the services Piranha Technologies offers",
             projTitle: "Piranha Technologies",
-            altTitle: "Piranha Technologies Image"
+            altTitle: "Piranha Technologies Image",
+            gitLink: "https://github.com/StevenS125/piranhatech",
+            siteLink: "https://www.piranhatechnologies.com/#/"
+
           })
       }
 
@@ -77,7 +81,9 @@ class Projects extends Component {
           projImg: "./STTDisplay.png",
           projDesc: "Straight Talk Today is built with a React Front End and a Node/Python Backend. This sites purpose was to promote Kevin Holmes Motivational Speaking Brand and announce his upcoming events to the world",
           projTitle: "Straight Talk Today",
-          altTitle: "Straight Talk Today Display Image"
+          altTitle: "Straight Talk Today Display Image",
+          gitLink: "https://github.com/StevenS125/straighttalktoday",
+          siteLink: "http://www.straighttalktoday.com/#/"
         })
     }
 
@@ -86,7 +92,9 @@ class Projects extends Component {
           projImg: "./contactDisplay.png",
           projDesc: "This CRM application was built with the MEAN Stack also know as MongoDB, Express and Node to manage our backened and Angular for the Front End Architecture. The purpose of this application is a Contact Management system that could be implemented in many devices",
           projTitle: "Contact Manager",
-          altTitle: "Contact Manager Display Image"
+          altTitle: "Contact Manager Display Image",
+          gitLink: "https://github.com/StevenS125/ContactManager",
+          siteLink: "https://github.com/StevenS125/ContactManager"
         })
     }
 
@@ -95,7 +103,9 @@ class Projects extends Component {
           projImg: "./forexDisplay.png",
           projDesc: "This application was made to handle currency trades to measure exchange rates and profit/loss overtime to keep track of accounts for foreign currency trading. It is built with MongoDB, Express, React and Node.JS",
           projTitle: "Foreign Exchange Trading App",
-          altTitle: "Foreign Exchange Application Display"
+          altTitle: "Foreign Exchange Application Display",
+          gitLink: "https://github.com/StevenS125/fxMan",
+          siteLink: "https://thawing-gorge-29999.herokuapp.com/#/login?_k=53jw1c"
         })
     }
 
@@ -104,7 +114,9 @@ class Projects extends Component {
           projImg: "./PTechDisplay.png",
           projDesc: "Selecting a college can be difficult, with this application the goal was to provide your preferences and search our database based on ranking to pick the perfect school. It is built with Express, JavaScript and SQL",
           projTitle: "College Selection Application",
-          altTitle: "College Selection Application Display Image"
+          altTitle: "College Selection Application Display Image",
+          gitLink: "https://github.com/StevenS125/collegeApp",
+          siteLink: "https://collegespot.herokuapp.com/"
         })
     }
 
@@ -113,7 +125,9 @@ class Projects extends Component {
           projImg: "./KKDisplay.png",
           projDesc: "Kim's Kustom's is a full service furniture repair business that needed help with a marketing site. It was created with a create-react-app format with a Node backend",
           projTitle: "Kim's Kustoms",
-          altTitle: "Kim's Customs Display Image"
+          altTitle: "Kim's Customs Display Image",
+          gitLink: "https://github.com/StevenS125/kimskustoms",
+          siteLink: "https://kimskustoms.herokuapp.com/"
         })
     }
 
