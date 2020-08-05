0af240218efeabffde870c9516fbacb2d701c329
Steven Snyder <sts12589@gmail.com>
2020-08-04 20:31:04 -0400

    overwrite old portfolio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Steve Snyder's <br></br>
+        New Portfolio Page</h1>
+        <h2>Coming Soon</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
