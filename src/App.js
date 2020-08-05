41569d8367f1f656818b8aed1657bd58670b91c0
Steven Snyder <sts12589@gmail.com>
2020-08-04 20:43:20 -0400

    overwrote page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
-import React, { Component } from 'react';
-// import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// Animate.css import for scrollAnimation
-import "animate.css/animate.min.css";
-import styles from './App.module.scss';
-
-
-//components
-import Nav from './Nav/Nav';
-import Landing from './Landing/Landing';
-import About from './About/About';
-import Profile from './Profile/Profile';
-import Projects from './Projects/Projects';
-import Contact from './Contact/Contact';
-import Footer from './Footer/Footer';
-//modules
-
-
-// Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebook, faTwitter, faLinkedin, faJsSquare, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faPalette, faCode, faLaptop, faArrowCircleRight, faTachometerAlt, faUserCheck } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faPalette, faCode, faLaptop, faGithub, faFacebook, faTwitter, faLinkedin, faArrowCircleRight, faTachometerAlt, faUserCheck, faJsSquare )
-
-
-
-class App extends Component {
-  render() {
-    return (
-         <div className={styles.container}>
-<Landing />
-<Nav />  
-<About />
-<Profile />
-<Projects />
-<Contact />
-<Footer />
-</div>
-    //   <Router>
-    //     <div >
-    //       <Switch>
-    //     <Route exact path="/" component={Landing}/>
-    //     </Switch>
-    // </div>
-    // </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Steve Snyder's <br></br>
+        New Portfolio Page</h1>
+        <h2>Coming Soon</h2>
+      </header>
+    </div>
+  );
 }
 
 export default App;
