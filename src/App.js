ce6e8839bc17a80edb5d54d1238040d547a44c1a
Steven Snyder <sts12589@gmail.com>
2020-08-05 21:53:55 -0400

    Added Wireframe and basic layout wireframe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Album from './Components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Steve Snyder's <br></br>
-        New Portfolio Page</h1>
-        <h2>Coming Soon</h2>
-      </header>
+    <div >
+        <Album />
     </div>
   );
 }
